5b81362c9afb7faa305f5b6a394321aa9eafe06e
Gitau Kiarie <igitaukiarie@gmail.com>
2023-01-24 17:48:39 +0000

    task 3

--- sort.h
+++ sort.h
@@ -4,9 +4,7 @@
 #include <stddef.h>
 
 /**
- * Header File
- * Contains prototypes for
- * Sorting_algorithms dir
+ * Header File - Contains prototypes for sorting algorithms directory
  */
 
 /**
@@ -18,15 +16,20 @@
  */
 typedef struct listint_s
 {
-  const int n;
-  struct listint_s *prev;
-  struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
+int algo_partition(int *array, size_t size, int low, int high);
+void algo_quick_sort(int *array, size_t size, int low, int high);
+void quick_sort(int *array, size_t size);
 
 #endif
